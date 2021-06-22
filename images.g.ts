be871be4097b51a7c11ffc3df0ba778af62b761a
Lytyt <84725314+Lytyt@users.noreply.github.com>
2021-06-22 09:10:12 +0800

    MakeCode 專案的初始檔案

---
+++ images.g.ts
@@ -0,0 +1,142 @@
+// 自動生成的代碼。請勿編輯。
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image3":
+            case "ballbal":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f b f f . . . . . . 
+. . . . f f b f f f f . . . . . 
+. . . f f b f f f f f f . . . . 
+. . f f f c f c f c f f f . . . 
+. f f f c f f f f f c b f f . . 
+. f f f f f f c f f f f b f . . 
+. b f f c f c f c f c f f b . . 
+. f b f f f f c f f f f f f . . 
+. f f b c f f f f f c f f f . . 
+. . f f f c f c f c f f f . . . 
+. . . f f f f f f b f f . . . . 
+. . . . f f f f b f f . . . . . 
+. . . . . f f b f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image4":
+            case "ballbal0":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f b f f . . . . . . 
+. . . . f f b f f f f . . . . . 
+. . . f f b f f f f f f . . . . 
+. . f f f c f c f c f f f . . . 
+. f f f c f f f f f c b f f . . 
+. f f f f f f c f f f f b f . . 
+. b f f c f c f c f c f f b . . 
+. f b f f f f c f f f f f f . . 
+. f f b c f f f f f c f f f . . 
+. . f f f c f c f c f f f . . . 
+. . . f f f f f f b f f . . . . 
+. . . . f f f f b f f . . . . . 
+. . . . . f f b f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "myAnim":
+            case "anim1":return [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f b f f . . . . . . 
+. . . . f f b f f f f . . . . . 
+. . . f f b f f f f f f . . . . 
+. . f f f c f c f c f f f . . . 
+. f f f c f f f f f c b f f . . 
+. f f f f f f c f f f f b f . . 
+. b f f c f c f c f c f f b . . 
+. f b f f f f c f f f f f f . . 
+. f f b c f f f f f c f f f . . 
+. . f f f c f c f c f f f . . . 
+. . . f f f f f f b f f . . . . 
+. . . . f f f f b f f . . . . . 
+. . . . . f f b f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f b f f . . . . . . 
+. . . . f f b f f f f . . . . . 
+. . . f f b f f f f f f . . . . 
+. . f f c f c f c f c f f . . . 
+. f f f f f f f f f f b f f . . 
+. f f f f f c c c f f f b f . . 
+. b f f c f c f c f c f f b . . 
+. f b f f f c c c f f f f f . . 
+. f f b f f f f f f f f f f . . 
+. . f f c f c f c f c f f . . . 
+. . . f f f f f f b f f . . . . 
+. . . . f f f f b f f . . . . . 
+. . . . . f f b f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f b f f . . . . . . 
+. . . . f f f f f f f . . . . . 
+. . . f f f f b f f f f . . . . 
+. . f f f c f c f c f f f . . . 
+. f f f c f f f f f c f f f . . 
+. f f f f f f c f f f f f f . . 
+. b f b c f c f c f c b f b . . 
+. f f f f f f c f f f f f f . . 
+. f f f c f f f f f c f f f . . 
+. . f f f c f c f c f f f . . . 
+. . . f f f f b f f f f . . . . 
+. . . . f f f f f f f . . . . . 
+. . . . . f f b f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f b f f . . . . . . 
+. . . . f f f f b f f . . . . . 
+. . . f f f f f f b f f . . . . 
+. . f f c f f c f f c f f . . . 
+. f f b f f f f f f f f f f . . 
+. f b f c f c f c f c f f f . . 
+. b f f f f f c f f f f f b . . 
+. f f f c f c f c f c f b f . . 
+. f f f f f f f f f f b f f . . 
+. . f f c f f c f f c f f . . . 
+. . . f f b f f f f f f . . . . 
+. . . . f f b f f f f . . . . . 
+. . . . . f f b f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f b f f . . . . . . 
+. . . . f f f f b f f . . . . . 
+. . . f f f f f f b f f . . . . 
+. . f f f c f c f c f f f . . . 
+. f f b c f f f f f c f f f . . 
+. f b f f f f c f f f f f f . . 
+. b f f c f c f c f c f f b . . 
+. f f f f f f c f f f f b f . . 
+. f f f c f f f f f c b f f . . 
+. . f f f c f c f c f f f . . . 
+. . . f f b f f f f f f . . . . 
+. . . . f f b f f f f . . . . . 
+. . . . . f f b f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+`];
+        }
+        return null;
+    })
+
+}
+// 自動生成的代碼。請勿編輯。
